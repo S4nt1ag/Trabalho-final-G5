4dda0ffc75640f993266ba3f94e2c0d70d9444a8
Gabriel Santiago Neves <gabrielsantiagoneves2@gmail.com>
2023-06-29 13:14:54 -0300

    salvando e deletando juntos

--- rota-dos-livros/src/components/ItemCarrinho.js
+++ rota-dos-livros/src/components/ItemCarrinho.js
@@ -1,33 +1,70 @@
-import React, { useEffect, useState, useContext} from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { Foundation } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 import { removeItenCart, deleteItemUni } from '../services/DataServices';
 import { CartContext } from '../context/CartContext';
+import * as SecureStore from 'expo-secure-store';
 
 export const ItemCarrinho = (props) => {
-
-
     const [qtd, setQtd] = useState(1);
     const [valorUni, setValorUni] = useState(20);
-    const { addToCart, qntdCarrinho, removeFromCart } = useContext(CartContext)
+    const { addToCart, qntdCarrinho, removeFromCart } = useContext(CartContext);
+
+    useEffect(() => {
+        retrieveData(); 
+    }, []);
+
+    const retrieveData = async () => {
+        try {
+            const savedQtd = await SecureStore.getItemAsync(`qtd_${props.id}`);
+            const savedValorUni = await SecureStore.getItemAsync(`valorUni_${props.id}`);
+
+            if (savedQtd !== null && savedValorUni !== null) {
+                setQtd(parseInt(savedQtd));
+                setValorUni(parseInt(savedValorUni));
+            }
+        } catch (error) {
+            console.log('Error retrieving data from SecureStore:', error);
+        }
+    };
+
+    const saveData = async () => {
+        try {
+            await SecureStore.setItemAsync(`qtd_${props.id}`, qtd.toString());
+            await SecureStore.setItemAsync(`valorUni_${props.id}`, valorUni.toString());
+        } catch (error) {
+            console.log('Error saving data to SecureStore:', error);
+        }
+    };
+
+    const deleteItem = async () => {
+        try {
+            await SecureStore.deleteItemAsync(`qtd_${props.id}`);
+            await SecureStore.deleteItemAsync(`valorUni_${props.id}`);
+            deleteItemUni('carrinho', props.id);
+        } catch (error) {
+            console.log('Error deleting data from SecureStore:', error);
+        }
+    };
 
     const decrementarQuantidade = () => {
         if (qtd > 1 && valorUni > 20) {
             setQtd(qtd - 1);
             setValorUni(valorUni - 20);
-            removeFromCart(1)
-            console.log(qntdCarrinho)
+            removeFromCart(1);
         }
     };
 
     const incrementarQuantidade = () => {
         setQtd(qtd + 1);
         setValorUni(valorUni + 20);
-        addToCart()
-        console.log(qntdCarrinho)
+        addToCart();
     };
 
+    useEffect(() => {
+        saveData();
+    }, [qtd, valorUni]);
 
     return (
         <View style={styles.containerLivroCarrinho}>
@@ -42,25 +79,21 @@ export const ItemCarrinho = (props) => {
             </View>
             <View>
                 <View style={styles.containerPrecoLivro}>
-
                     <View style={styles.qntButton}>
                         <TouchableOpacity onPress={() => decrementarQuantidade()}>
                             <Entypo name="minus" size={16} color="#fff" />
                         </TouchableOpacity>
 
                         <View>
-                            <Text style={{ color: "#fff" }}>{qtd}</Text>
+                            <Text style={{ color: '#fff' }}>{qtd}</Text>
                         </View>
 
                         <TouchableOpacity onPress={() => incrementarQuantidade()}>
                             <Entypo name="plus" size={16} color="#fff" />
                         </TouchableOpacity>
-
                     </View>
-                    <Text style={{ fontWeight: 'bold', fontSize: 13 }}>
-                        R$ {valorUni},00
-                    </Text>
-                    <TouchableOpacity onPress={() => deleteItemUni('carrinho', props.id)}>
+                    <Text style={{ fontWeight: 'bold', fontSize: 13 }}>R$ {valorUni},00</Text>
+                    <TouchableOpacity onPress={() => deleteItem()}>
                         <Foundation name="trash" size={22} color="black" />
                     </TouchableOpacity>
                 </View>
@@ -69,7 +102,6 @@ export const ItemCarrinho = (props) => {
     );
 };
 
-
 const styles = StyleSheet.create({
     containerLivroCarrinho: {
         flex: 1,
@@ -77,7 +109,7 @@ const styles = StyleSheet.create({
         gap: 10,
         alignItems: 'center',
         marginTop: 10,
-        marginBottom: 10
+        marginBottom: 10,
     },
     containerInfoLivro: {
         flex: 2,
@@ -85,7 +117,7 @@ const styles = StyleSheet.create({
     containerPrecoLivro: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
     },
     txtBtnQtd: {
         fontWeight: 'bold',
@@ -96,7 +128,7 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: 'bold',
         color: '#fff',
-        width: '100%'
+        width: '100%',
     },
     qntButton: {
         backgroundColor: '#116a7b',
@@ -104,6 +136,6 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         padding: 5,
         gap: 20,
-        borderRadius: 5
-    }
-})
+        borderRadius: 5,
+    },
+});
--- rota-dos-livros/src/context/CartContext.js
+++ rota-dos-livros/src/context/CartContext.js
@@ -3,25 +3,29 @@ import React, { createContext, useState } from 'react';
 export const CartContext = createContext({});
 
 export const CartProvider = ({ children }) => {
-
     const [qntdCarrinho, setQntdCarrinho] = useState(0);
 
     const addToCart = () => {
-        setQntdCarrinho(qntdCarrinho + 1)
-    }
+        setQntdCarrinho(qntdCarrinho + 1);
+    };
 
     const removeFromCart = (qntd) => {
-        if (qntdCarrinho > 0 && qntd < qntdCarrinho)
-            setQntdCarrinho(qntdCarrinho - qntd)
-    }
+        if (qntdCarrinho > 0 && qntd < qntdCarrinho) {
+            setQntdCarrinho(qntdCarrinho - qntd);
+        } else {
+            setQntdCarrinho(0); // Zera a quantidade no carrinho
+        }
+    };
 
     return (
-        <CartContext.Provider value={{
-            qntdCarrinho,
-            addToCart,
-            removeFromCart
-        }}>
+        <CartContext.Provider
+            value={{
+                qntdCarrinho,
+                addToCart,
+                removeFromCart,
+            }}
+        >
             {children}
         </CartContext.Provider>
-    )
-}
+    );
+};
